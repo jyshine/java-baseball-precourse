f6919fa7a5720bc4c75b2a01db68d02d6ceaa54d
june <june@thecommerce.co.kr>
2022-10-03 16:39:19 +0900

    refactor(ballStatus): 불피요한 key 삭제

--- src/main/java/baseball/common/BallStatus.java
+++ src/main/java/baseball/common/BallStatus.java
@@ -1,15 +1,13 @@
 package baseball.common;
 
 public enum BallStatus {
-    NOTHING("nothing", "낫싱"),
-    BALL("ball", "볼"),
-    STRIKE("strike", "스트라이크");
+    NOTHING("낫싱"),
+    BALL("볼"),
+    STRIKE("스트라이크");
 
-    private final String key;
     private final String status;
 
-    BallStatus(String key, String status) {
-        this.key = key;
+    BallStatus(String status) {
         this.status = status;
     }
 
